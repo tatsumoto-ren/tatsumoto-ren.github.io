728b4e5bfeedd320cf6aa2d33798d2f07a19c67d
Ren Tatsumoto <tatsu@autistici.org>
2022-02-14 10:34:38 +0300

    use this for self reference

--- blog/res/blog.js
+++ blog/res/blog.js
@@ -41,7 +41,7 @@ const Utils = Object.freeze({
         return window.location.pathname.split('/').slice(-1).join('')
     },
     is_tags_page() {
-        return Utils.filename().startsWith('tag_')
+        return this.filename().startsWith('tag_')
     },
     query_headers(parent) {
         const headers = parent.querySelectorAll(':is(h1, h2, h3, h4, h5, h6)')
@@ -71,15 +71,16 @@ const Utils = Object.freeze({
 const Sidebar = Object.freeze({
     id: 'sidebar',
     btn_id: 'show_sidebar_button',
+    open_inline_svg: `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32.055 32.055" xml:space="preserve"><path d="M3.968,12.061C1.775,12.061,0,13.835,0,16.027c0,2.192,1.773,3.967,3.968,3.967c2.189,0,3.966-1.772,3.966-3.967 C7.934,13.835,6.157,12.061,3.968,12.061z M16.233,12.061c-2.188,0-3.968,1.773-3.968,3.965c0,2.192,1.778,3.967,3.968,3.967 s3.97-1.772,3.97-3.967C20.201,13.835,18.423,12.061,16.233,12.061z M28.09,12.061c-2.192,0-3.969,1.774-3.969,3.967 c0,2.19,1.774,3.965,3.969,3.965c2.188,0,3.965-1.772,3.965-3.965S30.278,12.061,28.09,12.061z"/></svg>`,
+    up_inline_svg: `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><path d="M64.8,554.8l102.8-102.9l259.7,259.8V10h145.4v701.7l259.7-259.8l102.8,102.9L500.1,990L64.8,554.8z"/></svg>`,
     make_sidebar_header() {
-        const up_inline_svg = () => { return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><g><path d="M64.8,554.8l102.8-102.9l259.7,259.8V10h145.4v701.7l259.7-259.8l102.8,102.9L500.1,990L64.8,554.8z"/></g></svg>`; }
         return [
             `<div class="sidebar_header">`,
             `<a href="table-of-contents.html" class="to_toc" title="Open Table of Contents">Table of Contents</a>`,
-            `<a href="#" onclick="Sidebar.close()" class="jump_top_button" title="Go to top">${up_inline_svg()}</a>`,
+            `<a href="#" onclick="Sidebar.close()" class="jump_top_button" title="Go to top">${this.up_inline_svg}</a>`,
             `<a href="javascript:void(0)" onclick="Sidebar.close()" class="close_button" title="Close">×</a>`,
             `</div>`
-        ].join('')
+        ].join('');
     },
     make_list(headers) {
         let prev_level = 0
@@ -107,37 +108,36 @@ const Sidebar = Object.freeze({
         return list
     },
     make_open_button() {
-        const inline_svg = () => { return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32.055 32.055" xml:space="preserve"><path d="M3.968,12.061C1.775,12.061,0,13.835,0,16.027c0,2.192,1.773,3.967,3.968,3.967c2.189,0,3.966-1.772,3.966-3.967 C7.934,13.835,6.157,12.061,3.968,12.061z M16.233,12.061c-2.188,0-3.968,1.773-3.968,3.965c0,2.192,1.778,3.967,3.968,3.967 s3.97-1.772,3.97-3.967C20.201,13.835,18.423,12.061,16.233,12.061z M28.09,12.061c-2.192,0-3.969,1.774-3.969,3.967 c0,2.19,1.774,3.965,3.969,3.965c2.188,0,3.965-1.772,3.965-3.965S30.278,12.061,28.09,12.061z"/></svg>`; }
         const btn = document.createElement('button')
-        btn.onclick = Sidebar.toggle
-        btn.id = Sidebar.btn_id
+        btn.onclick = this.toggle.bind(this)
+        btn.id = this.btn_id
         btn.title = "Open sidebar"
-        btn.innerHTML = inline_svg()
+        btn.innerHTML = this.open_inline_svg
         return btn
     },
     create(headers) {
         const div = document.createElement('div')
-        div.id = Sidebar.id
-        div.innerHTML = Sidebar.make_sidebar_header() + Sidebar.make_list(headers)
+        div.id = this.id
+        div.innerHTML = this.make_sidebar_header() + this.make_list(headers)
         return div
     },
     close() {
-        Utils.maybe_toggle_attr($(Sidebar.id), 'open', false)
-        Utils.maybe_toggle_attr($(Sidebar.btn_id), 'open', false)
+        Utils.maybe_toggle_attr($(this.id), 'open', false)
+        Utils.maybe_toggle_attr($(this.btn_id), 'open', false)
     },
     toggle() {
-        Utils.maybe_toggle_attr($(Sidebar.id), 'open')
-        Utils.maybe_toggle_attr($(Sidebar.btn_id), 'open')
+        Utils.maybe_toggle_attr($(this.id), 'open')
+        Utils.maybe_toggle_attr($(this.btn_id), 'open')
         $('menu-btn').checked = false
     },
     init() {
         if (!Utils.is_index() && !Utils.is_tags_page()) {
             const headers = Utils.query_headers($('divbody')).filter(h => Boolean(h.id))
             if (headers.length > 0) {
-                document.body.append(Sidebar.create(headers))
-                document.querySelector('header>.logoholder').append(Sidebar.make_open_button())
-                document.querySelector('label.menu').addEventListener('click', Sidebar.close)
-                $('divbody').addEventListener('click', Sidebar.close)
+                document.body.append(this.create(headers))
+                document.querySelector('header>.logoholder').append(this.make_open_button())
+                document.querySelector('label.menu').addEventListener('click', this.close.bind(this))
+                $('divbody').addEventListener('click', this.close.bind(this))
             }
         }
     }
@@ -189,16 +189,16 @@ const Toc = Object.freeze({
         return div
     },
     link_adjacent() {
-        const i = Toc.order.indexOf(Utils.filename())
+        const i = this.order.indexOf(Utils.filename())
         const outer = document.querySelector('div#divbody')
         if (i >= 0 && outer) {
-            const container = outer.appendChild(Toc.make_nav_container())
-            const [prev, next] = [Toc.order[i - 1], Toc.order[i + 1]]
+            const container = outer.appendChild(this.make_nav_container())
+            const [prev, next] = [this.order[i - 1], this.order[i + 1]]
             if (prev !== undefined) {
-                container.appendChild(Toc.make_link(prev, false))
+                container.appendChild(this.make_link(prev, false))
             }
             if (next !== undefined) {
-                container.appendChild(Toc.make_link(next, true))
+                container.appendChild(this.make_link(next, true))
             }
         }
     }
