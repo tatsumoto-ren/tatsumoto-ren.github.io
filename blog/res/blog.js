8cbfa30ee732342504216d62b4f5296046b313e4
Ren Tatsumoto <tatsu@autistici.org>
2022-11-24 22:31:57 +0300

    update blog css
    move mega link image to css

--- blog/res/blog.js
+++ blog/res/blog.js
@@ -199,9 +199,8 @@ const MegaTags = Object.freeze({
         mega_tag.classList.add("mega_link")
         mega_tag.target = "_blank"
         mega_tag.title = "Use megatools"
-        mega_tag.style = "margin-left: 4px;"
         mega_tag.href = "https://aur.archlinux.org/packages/megatools"
-        mega_tag.innerHTML = '<img alt="mega" style="vertical-align: -0.125em; height: 1em;" src="https://avatars.githubusercontent.com/u/4920706?s=200&v=4">'
+        mega_tag.innerHTML = ""
         return mega_tag
     },
     mark_links() {
