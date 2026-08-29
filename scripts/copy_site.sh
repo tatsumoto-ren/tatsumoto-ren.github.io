6dde08fe81853900b01735a26ab00d4ed0a8faff
Ren Tatsumoto <tatsu@autistici.org>
2026-08-29 20:29:27 +0300

    scripts: copy russian site
    copy site: update comment

--- scripts/copy_site.sh
+++ scripts/copy_site.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 set -euo pipefail
+shopt -s nullglob
 
 # shellcheck source=scripts/lib.sh
 . "$(dirname -- "$0")/lib.sh"
 
+# Validate a clean repository and copy only deployable static assets into _site.
 main() {
 	ensure_git_repo
 	git_no_differences || die "working tree is dirty"
 	rm -rf -- _site || die "can't remove site dir"
 	mkdir -p -- _site || die "can't create site dir"
+	# Exclude vitest.config.js from deployment.
 	cp -v -R --parents -- \
-		./index.* \
-		./img \
-		./res \
-		./not_found.* \
+		./*.html \
 		./robots.txt \
 		./sitemap.xml \
-		./blog/*.{html,md,rss,json} \
-		./blog/{aud,img,res,vid}/* \
+		./{img,res}/* \
+		./{blog,ru}/*.{html,md,rss,json} \
+		./{blog,ru}/{aud,img,res,vid}/* \
 		_site/
 	echo "Done."
 }
